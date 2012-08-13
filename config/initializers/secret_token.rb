72551ce81922356e9b0ac38133278e0b638c98cc
Evan Prothro <evan.prothro@lifechurch.tv>
2012-08-13 17:19:13 -0500

    base refinerycms app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProtegeLsm::Application.config.secret_token = '1ba761700d529c40917193adfea08419ec34ce82f2ec6e20b3fff51c23c0ac7eec760dbb1414833657b04245f571531cedb7f4dd56858b146f8e6c93525fd14e'
