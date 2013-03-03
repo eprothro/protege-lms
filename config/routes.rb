23646b8133e923db7e01c3d6d157a4406c3970ab
Evan Prothro <evan.prothro@gmail.com>
2013-03-03 12:10:05 -0600

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 ProtegeLms::Application.routes.draw do
 
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root to: 'pages#welcome'
+  get "/about" => 'pages#about', as: 'about'
+
   devise_for :users
 
   resources :users, only: [:show, :edit, :update]
@@ -30,56 +35,6 @@ ProtegeLms::Application.routes.draw do
   resources :series
   resources :courses, :lessons, :quizzes, :questions, :answers, :tips, except: :new
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'pages#welcome'
 
   # See how all your routes lay out with "rake routes"
 
