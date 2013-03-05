07fd0d850f5af49e6b14d91f4d5cd32978181b84
Evan Prothro <evan.prothro@gmail.com>
2013-03-04 19:18:02 -0600

    email field to text field

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ ProtegeLms::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => ENV["EMAIL_HOST"] || 'www.protege-lms.herokuappg.com' }
+  config.action_mailer.default_url_options = { :host => ENV["EMAIL_HOST"] || 'www.protege-lms.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
